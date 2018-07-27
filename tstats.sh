c92b5688942ee0d25d3306d60704cf060460625d
aloth <home@alexloth.com>
2018-07-27 19:12:32 +0200

    Save data as local CSV file as backup
    Save data as local CSV file as backup

--- tstats.sh
+++ tstats.sh
@@ -16,5 +16,11 @@ IFS=',' read -r -a array <<< "$stats"
 # Form JSON
 json="{\"value1\":\"${array[3]}\",\"value2\":\"${array[6]}\",\"value3\":\"${array[7]}\"}"
 
+# Get formatted timestamp
+timestamp=`date '+%Y-%m-%d %H:%M:%S'`
+
+# Append to local CSV file
+echo "$timestamp, $screenname, ${array[3]}, ${array[6]}, ${array[7]}" >> tstats.csv
+
 # Send to IFTTT
 curl -X POST -H "Content-Type: application/json" -d "${json}" https://maker.ifttt.com/trigger/${trigger}/with/key/${secret}
